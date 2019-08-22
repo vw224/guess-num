6bc051fb051672003845e8ad10381d07c2e9f24e
Vivi <vvw224@gmail.com>
2019-08-22 11:18:44 +0800

    version 2.0

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,17 @@
 """
-產生一個隨機整數(1~100) (不要印出來)
+讓使用者決定一個範圍，產生一個隨機整數(不要印出來)
 讓使用者重複輸入數字去猜，並且印出 猜第幾次
 猜對的話，印出"終於猜對了!"
 猜錯的話，要告訴他 比答案大/小
 """
 
-
 import random
-num = random.randint(1, 100)
-#print(num)
+
+x = int(input("請輸入起始值: "))
+y = int(input("請輸入終止值: "))
+
+num = random.randint(x, y)
+#print(pwd)
 
 pwd = int(input("請輸入密碼: "))
 count = 0
@@ -23,12 +26,15 @@ while pwd != num:
 		pwd = int(input("請輸入密碼: "))
 print("終於猜對了! 總共猜",count,"次", sep="")
 
-
-
-
 """
+參考解答:
 import random
-r = random.randint(1, 100)
+start = input("請決定隨機數字範圍開始值: ")
+end = input("請決定隨機數字範圍結束值: ")
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 
 while True:
@@ -37,6 +43,7 @@ while True:
 	num = int(num)
 	if num == r:
 		print("你猜中了!")
+		print("這是你猜的第", count, "次")
 		break
 	elif num > r:
 		print("比答案大")
